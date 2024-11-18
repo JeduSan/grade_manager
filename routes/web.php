efd12ae67d2463eaa0d89791bde91e5a04e6a1c9
JeduSan <jedbenedictlba@gmail.com>
2024-11-18 20:47:07 +0800

    added admin, teacher middlewares

--- routes/web.php
+++ routes/web.php
@@ -30,7 +30,7 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
 
 /**
  * =============================================
@@ -41,66 +41,72 @@ require __DIR__.'/auth.php';
  */
 
 // TEMP LANDING PAGE
-Route::get('/',function () {
+Route::get('/', function () {
     // return view('landing');
     // REVIEW: Our landing page will be the login page
     return to_route('login');
+})->middleware(['guest']);
+
+// ==========
+// ADMIN SIDE
+// ==========
+Route::middleware(['auth', 'verified', 'admin'])->group(function () {
+    Route::get('/admin/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
+    // Route::post('/admin/dashboard/add/teacher',[AddTeacherController::class,'store'])->name('admin.dashboard.add.teacher');
+    // Route::post('/admin/dashboard/add/student',[AddStudentController::class,'store'])->name('admin.dashboard.add.student');
+    // Route::post('/admin/dashboard/add/subject',[AddSubjectController::class,'store'])->name('admin.dashboard.add.subject');
+    // Route::post('/admin/dashboard/add/semester',[AddSemesterController::class,'store'])->name('admin.dashboard.add.semester');
+
+    // DEPRECATED
+    // Route::get('/admin/manager',[ManagerController::class,'index'])->name('admin.manager');
+
+    // TEACHER MANAGER
+    Route::get('/admin/manager/teacher', [ManagerTeacherController::class, 'index'])->name('admin.manager.teacher');
+    Route::post('/admin/manager/add/teacher', [ManagerTeacherController::class, 'store'])->name('admin.manager.add.teacher');
+    Route::post('/admin/manager/add/teacher/excel', [TeacherExcelController::class, 'store'])->name('admin.manager.add.teacher.excel');
+    Route::get('/admin/manager/delete/teacher/{id}', [ManagerTeacherController::class, 'destroy'])->name('admin.manager.delete.teacher');
+    Route::patch('/admin/manager/edit/teacher/{user_id}', [ManagerTeacherController::class, 'update'])->name('admin.manager.edit.teacher');
+
+    // STUDENT MANAGER
+    Route::get('/admin/manager/student', [ManagerStudentController::class, 'index'])->name('admin.manager.student');
+    Route::post('/admin/manager/add/student', [ManagerStudentController::class, 'store'])->name('admin.manager.add.student');
+    Route::post('/admin/manager/add/student/excel', [StudentExcelController::class, 'store'])->name('admin.manager.add.student.excel');
+    Route::get('/admin/manager/delete/student/{id}', [ManagerStudentController::class, 'destroy'])->name('admin.manager.delete.student');
+    Route::patch('/admin/manager/edit/student/{user_id}', [ManagerStudentController::class, 'update'])->name('admin.manager.edit.student');
+
+    // SEMESTER MANAGER
+    Route::get('/admin/manager/semester', [ManagerSemesterController::class, 'index'])->name('admin.manager.semester');
+    Route::post('/admin/manager/add/semester', [ManagerSemesterController::class, 'store'])->name('admin.manager.add.semester');
+    Route::get('/admin/manager/delete/semester/{id}', [ManagerSemesterController::class, 'destroy'])->name('admin.manager.delete.semester');
+    Route::patch('/admin/manager/edit/semester/{id}', [ManagerSemesterController::class, 'update'])->name('admin.manager.edit.semester');
+
+    // SUBJECT MANAGER
+    Route::get('/admin/manager/subject', [ManagerSubjectController::class, 'index'])->name('admin.manager.subject');
+    Route::post('/admin/manager/add/subject', [ManagerSubjectController::class, 'store'])->name('admin.manager.add.subject');
+    Route::post('/admin/manager/add/subject/excel', [SubjectExcelController::class, 'store'])->name('admin.manager.add.subject.excel');
+    Route::get('/admin/manager/delete/subject/{id}', [ManagerSubjectController::class, 'destroy'])->name('admin.manager.delete.subject');
+    Route::patch('/admin/manager/edit/subject/{id}', [ManagerSubjectController::class, 'update'])->name('admin.manager.edit.subject');
+
+    // CLASS MANAGER
+    Route::get('/admin/manager/class', [ManagerClassController::class, 'index'])->name('admin.manager.class');
+    Route::post('/admin/manager/add/class', [ManagerClassController::class, 'store'])->name('admin.manager.add.class');
+    Route::patch('/admin/manager/edit/class/{id}', [ManagerClassController::class, 'update'])->name('admin.manager.edit.class');
+    Route::get('/admin/manager/delete/class/{id}', [ManagerClassController::class, 'destroy'])->name('admin.manager.delete.class');
+    Route::get('/admin/manager/view/class/{id}', [ManagerClassController::class, 'show'])->name('admin.manager.show.class');
+    // CLASS MANAGER > VIEW CLASS
+    Route::get('/admin/manager/view/class/remove/student/{student_class_id}/{class_id}', [RemoveStudentFromClassController::class, 'destroy'])->name('admin.manager.view.class.remove.student');
+    Route::post('/admin/manager/view/class/add/student/{class_id}', [AddStudentFromClassController::class, 'store'])->name('admin.manager.view.class.add.student');
+
+    // USER MANAGER
+    Route::get('/admin/manager/user', [ManagerUserController::class, 'index'])->name('admin.manager.user');
+    Route::post('/admin/manager/add/user', [ManagerUserController::class, 'store'])->name('admin.manager.add.user');
+    Route::patch('/admin/manager/edit/user/{id}', [ManagerUserController::class, 'update'])->name('admin.manager.edit.user');
+    Route::get('/admin/manager/delete/user/{id}', [ManagerUserController::class, 'destroy'])->name('admin.manager.delete.user');
 });
 
-Route::get('/admin/dashboard',[DashboardController::class,'index'])->name('admin.dashboard');
-// Route::post('/admin/dashboard/add/teacher',[AddTeacherController::class,'store'])->name('admin.dashboard.add.teacher');
-// Route::post('/admin/dashboard/add/student',[AddStudentController::class,'store'])->name('admin.dashboard.add.student');
-// Route::post('/admin/dashboard/add/subject',[AddSubjectController::class,'store'])->name('admin.dashboard.add.subject');
-// Route::post('/admin/dashboard/add/semester',[AddSemesterController::class,'store'])->name('admin.dashboard.add.semester');
-
-// DEPRECATED
-// Route::get('/admin/manager',[ManagerController::class,'index'])->name('admin.manager');
-
-// TEACHER MANAGER
-Route::get('/admin/manager/teacher',[ManagerTeacherController::class,'index'])->name('admin.manager.teacher');
-Route::post('/admin/manager/add/teacher',[ManagerTeacherController::class,'store'])->name('admin.manager.add.teacher');
-Route::post('/admin/manager/add/teacher/excel',[TeacherExcelController::class,'store'])->name('admin.manager.add.teacher.excel');
-Route::get('/admin/manager/delete/teacher/{id}',[ManagerTeacherController::class,'destroy'])->name('admin.manager.delete.teacher');
-Route::patch('/admin/manager/edit/teacher/{user_id}',[ManagerTeacherController::class,'update'])->name('admin.manager.edit.teacher');
-
-// STUDENT MANAGER
-Route::get('/admin/manager/student',[ManagerStudentController::class,'index'])->name('admin.manager.student');
-Route::post('/admin/manager/add/student',[ManagerStudentController::class,'store'])->name('admin.manager.add.student');
-Route::post('/admin/manager/add/student/excel',[StudentExcelController::class,'store'])->name('admin.manager.add.student.excel');
-Route::get('/admin/manager/delete/student/{id}',[ManagerStudentController::class,'destroy'])->name('admin.manager.delete.student');
-Route::patch('/admin/manager/edit/student/{user_id}',[ManagerStudentController::class,'update'])->name('admin.manager.edit.student');
-
-// SEMESTER MANAGER
-Route::get('/admin/manager/semester',[ManagerSemesterController::class,'index'])->name('admin.manager.semester');
-Route::post('/admin/manager/add/semester',[ManagerSemesterController::class,'store'])->name('admin.manager.add.semester');
-Route::get('/admin/manager/delete/semester/{id}',[ManagerSemesterController::class,'destroy'])->name('admin.manager.delete.semester');
-Route::patch('/admin/manager/edit/semester/{id}',[ManagerSemesterController::class,'update'])->name('admin.manager.edit.semester');
-
-// SUBJECT MANAGER
-Route::get('/admin/manager/subject',[ManagerSubjectController::class,'index'])->name('admin.manager.subject');
-Route::post('/admin/manager/add/subject',[ManagerSubjectController::class,'store'])->name('admin.manager.add.subject');
-Route::post('/admin/manager/add/subject/excel',[SubjectExcelController::class,'store'])->name('admin.manager.add.subject.excel');
-Route::get('/admin/manager/delete/subject/{id}',[ManagerSubjectController::class,'destroy'])->name('admin.manager.delete.subject');
-Route::patch('/admin/manager/edit/subject/{id}',[ManagerSubjectController::class,'update'])->name('admin.manager.edit.subject');
-
-// CLASS MANAGER
-Route::get('/admin/manager/class',[ManagerClassController::class,'index'])->name('admin.manager.class');
-Route::post('/admin/manager/add/class',[ManagerClassController::class,'store'])->name('admin.manager.add.class');
-Route::patch('/admin/manager/edit/class/{id}',[ManagerClassController::class,'update'])->name('admin.manager.edit.class');
-Route::get('/admin/manager/delete/class/{id}',[ManagerClassController::class,'destroy'])->name('admin.manager.delete.class');
-Route::get('/admin/manager/view/class/{id}',[ManagerClassController::class,'show'])->name('admin.manager.show.class');
-// CLASS MANAGER > VIEW CLASS
-Route::get('/admin/manager/view/class/remove/student/{student_class_id}/{class_id}',[RemoveStudentFromClassController::class,'destroy'])->name('admin.manager.view.class.remove.student');
-Route::post('/admin/manager/view/class/add/student/{class_id}',[AddStudentFromClassController::class,'store'])->name('admin.manager.view.class.add.student');
-
-// USER MANAGER
-Route::get('/admin/manager/user',[ManagerUserController::class,'index'])->name('admin.manager.user');
-Route::post('/admin/manager/add/user',[ManagerUserController::class,'store'])->name('admin.manager.add.user');
-Route::patch('/admin/manager/edit/user/{id}',[ManagerUserController::class,'update'])->name('admin.manager.edit.user');
-Route::get('/admin/manager/delete/user/{id}',[ManagerUserController::class,'destroy'])->name('admin.manager.delete.user');
-
-
-// =========
+// ============
 // TEACHER SIDE
-// =========
-Route::get('/teacher/dashboard',[TeacherDashboardController::class,'index'])->name('teacher.dashboard');
+// ============
+Route::middleware(['auth','verified','teacher'])->group(function () {
+    Route::get('/teacher/dashboard', [TeacherDashboardController::class, 'index'])->name('teacher.dashboard');
+});
