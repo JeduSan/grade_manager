e02915e3bc06d30063e79c4d0d0d0187d7e7de66
JeduSan <jedbenedictlba@gmail.com>
2024-11-21 00:06:09 +0800

    Minor ui fixes

--- resources/views/components/admin_sidebar.blade.php
+++ resources/views/components/admin_sidebar.blade.php
@@ -1,40 +1,41 @@
 <!-- Sidebar -->
 <div class="col-auto p-0" id="sidebar">
-    <div class="d-flex flex-column align-items-center align-items-sm-start text-white min-vh-100">
-        <div class="logo-container">
+    <div class="text-white d-flex flex-column align-items-center align-items-sm-start min-vh-100">
+        {{-- <div class="logo-container">
             <br><br><br>
-        </div>
-        <button type="button" id="sidebarCollapse" class="btn btn-info">
+        </div> --}}
+
+        <button type="button" id="sidebarCollapse" class="btn btn-info" style="margin-top: 5rem;">
             <span class="navbar-toggler-icon"><i class="fa-solid fa-bars"></i></span>
         </button>
-        <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
+        <ul class="mb-0 nav nav-pills flex-column mb-sm-auto align-items-center align-items-sm-start" id="menu">
             <li>
-                <a href="/admin/dashboard" class="nav-link px-0 align-middle text-white">
+                <a href="/admin/dashboard" class="px-0 text-white align-middle nav-link">
                     <i class="fas fa-tachometer-alt icon-size"></i> <span class="ms-1">Dashboard</span>
                 </a>
             </li>
             <li>
-                <a href="/admin/manager/teacher" class="nav-link px-0 align-middle text-white">
+                <a href="/admin/manager/teacher" class="px-0 text-white align-middle nav-link">
                     <i class="fas fa-user icon-size"></i> <span class="ms-1">Manage Teachers</span>
                 </a>
             </li>
             <li>
-                <a href="/admin/manager/student" class="nav-link px-0 align-middle text-white">
+                <a href="/admin/manager/student" class="px-0 text-white align-middle nav-link">
                     <i class="fas fa-users icon-size"></i> <span class="ms-1">Manage Students</span>
                 </a>
             </li>
             <li>
-                <a href="/admin/manager/class" class="nav-link px-0 align-middle text-white">
+                <a href="/admin/manager/class" class="px-0 text-white align-middle nav-link">
                     <i class="fas fa-chalkboard-teacher icon-size"></i> <span class="ms-1">Manage Class</span>
                 </a>
             </li>
             <li>
-              <a href="/admin/manager/subject" class="nav-link px-0 align-middle text-white">
+              <a href="/admin/manager/subject" class="px-0 text-white align-middle nav-link">
                   <i class="fas fa-cogs icon-size"></i> <span class="ms-1">Manage Subjects</span>
               </a>
           </li>
             <li>
-                <a href="/admin/manager/user" class="nav-link px-0 align-middle text-white">
+                <a href="/admin/manager/user" class="px-0 text-white align-middle nav-link">
                     <i class="fas fa-cogs icon-size"></i> <span class="ms-1">Manage Users</span>
                 </a>
             </li>
--- resources/views/components/teacher_sidebar.blade.php
+++ resources/views/components/teacher_sidebar.blade.php
@@ -1,20 +1,21 @@
 <!-- Sidebar -->
 <div class="col-auto p-0" id="sidebar">
-    <div class="d-flex flex-column align-items-center align-items-sm-start text-white min-vh-100">
-        <div class="logo-container">
+    <div class="text-white d-flex flex-column align-items-center align-items-sm-start min-vh-100">
+        {{-- <div class="logo-container">
             <br><br><br>
-        </div>
-        <button type="button" id="sidebarCollapse" class="btn btn-info">
+        </div> --}}
+
+        <button type="button" id="sidebarCollapse" class="btn btn-info" style="margin-top: 5rem;">
             <span class="navbar-toggler-icon"><i class="fa-solid fa-bars"></i></span>
         </button>
-        <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
+        <ul class="mb-0 nav nav-pills flex-column mb-sm-auto align-items-center align-items-sm-start" id="menu">
             <li>
-                <a href="/teacher/dashboard" class="nav-link px-0 align-middle text-white">
+                <a href="/teacher/dashboard" class="px-0 text-white align-middle nav-link">
                     <i class="fas fa-tachometer-alt icon-size"></i> <span class="ms-1">Dashboard</span>
                 </a>
             </li>
             <li>
-                <a href="/teacher/view/subjects" class="nav-link px-0 align-middle text-white">
+                <a href="/teacher/view/subjects" class="px-0 text-white align-middle nav-link">
                     <i class="fas fa-user icon-size"></i> <span class="ms-1">View Subjects</span>
                 </a>
             </li>
